fda7c635f12246d6c50bdd5ac4c469ff0e5238c5
Rizwan <rizwan819101@gmail.com>
2025-07-27 13:54:06 -0700

    image comprese

--- src/compontents/SidBar.js
+++ src/compontents/SidBar.js
@@ -1,27 +1,29 @@
-import { useState } from "react"
- const Sidebar = () => {
-  const[state , setState]= useState([
-   {id:1, image:'./img/r1.PNG', name:`Adnan Khan`},
-   {id:2, image:'./img/r2.PNG', name:`Asad Khan`},
-   {id:3, image:'./img/r3.PNG', name:`Faizullah Khan`},
-   {id:4, image:'./img/r4.PNG', name:`Habib Ur Rehman`},
-   {id:5, image:'./img/r6.PNG', name:`Haq Nawaz Khan`},
-   {id:6, image:'./img/r8.PNG', name:`Kashif Khan`},
-   {id:6, image:'./img/r10.PNG', name:`Zeeshan Khan`},
-   {id:6, image:'./img/r9.PNG', name:`Kamran Khan`},
+import { useState } from "react";
+
+const Sidebar = () => {
+  const [state, setState] = useState([
+    { id: 1, image: './img/r1.webp', name: `Adnan Khan` },
+    { id: 2, image: './img/r2.webp', name: `Asad Khan` },
+    { id: 3, image: './img/r3.webp', name: `Faizullah Khan` },
+    { id: 4, image: './img/r4.webp', name: `Habib Ur Rehman` },
+    { id: 5, image: './img/r6.webp', name: `Haq Nawaz Khan` },
+    { id: 6, image: './img/r8.webp', name: `Kashif Khan` },
+    { id: 6, image: './img/r10.webp', name: `Zeeshan Khan` },
+    { id: 6, image: './img/r9.webp', name: `Kamran Khan` },
   ]);
 
-  return ( 
-  <div className='sidebar'> 
-  {state.map((info) => ( 
-    <div className='sidebar__list'key={info.id}>
-     <div className='show__header-img'>
-        <img src={info.image}alt='user'/>
-    </div>
-    <div className='show__header-name'>{info.name}
-    </div>
-    </div> 
-  ))}      
+  return (
+    <div className='sidebar'>
+      {state.map((info) => (
+        <div className='sidebar__list' key={info.id}>
+          <div className='show__header-img'>
+            <img src={info.image} alt={info.name} loading="lazy" />
+          </div>
+          <div className='show__header-name'>
+            {info.name}
+          </div>
+        </div>
+      ))}
     </div>
   );
 };
